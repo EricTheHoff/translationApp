f91e942539623bb6c88dcff5b030fbd5511c9f1f
AMY ANDREW <AMYRTANDREW@GMAIL.COM>
2023-12-05 11:08:52 -0700

    router and pages created

---
+++ src/Pages/SavedTranslationsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SavedTranslationsPage = () => {
+  return (
+    <div>
+      <Link to="/">Back to Home</Link>
+    </div>
+  );
+};
+
+export default SavedTranslationsPage;
---
+++ src/Pages/SavedTutorPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SavedTutorPage = () => {
+  return (
+    <div>
+      <Link to="/">Back to Home</Link>
+    </div>
+  );
+};
+
+export default SavedTutorPage;
