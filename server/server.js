cab39531a76a524bd123583bf4914cd5bb25e771
AMY ANDREW <AMYRTANDREW@GMAIL.COM>
2023-12-05 10:00:49 -0700

    server created

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import morgan from "morgan";
+import ViteExpress from "vite-express";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(express.json());
+
+ViteExpress.listen(app, 2222, () =>
+  console.log(`Server working on http://localhost:2222`)
+);
