3eaf85a61038941d448ec29b2fcbd0631a578ed5
AMY ANDREW <AMYRTANDREW@GMAIL.COM>
2023-12-07 13:57:34 -0700

    got rid of register bug

--- src/Pages/RegisterPage.jsx
+++ src/Pages/RegisterPage.jsx
@@ -5,12 +5,6 @@ import RegistrationForm from "../Components/RegistrationForm";
 const RegisterPage = () => {
   return (
     <div>
-      <APIProvider apiKey={apiKey}>
-        <div style={{ height: "100vh" }}>
-          <Map zoom={3} center={{ lat: 22.54992, lng: 0 }} mapId={mapId}></Map>
-        </div>
-      </APIProvider>
-
       <RegistrationForm />
 
       <Link to="/">Back to Home</Link>
