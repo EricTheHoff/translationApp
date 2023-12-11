92189689d7b68efc7f4863747c8b501576c3cc36
AMY ANDREW <AMYRTANDREW@GMAIL.COM>
2023-12-11 09:50:48 -0700

    added 32-highlight info

--- src/Components/PDFUpload.jsx
+++ src/Components/PDFUpload.jsx
@@ -1,74 +1,73 @@
-import { useState } from 'react'
-import React from 'react'
-import { Viewer, Worker } from '@react-pdf-viewer/core'
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
-import '@react-pdf-viewer/core/lib/styles/index.css'
-import '@react-pdf-viewer/default-layout/lib/styles/index.css'
-import { Link } from 'react-router-dom'
-
+import { useState } from "react";
+import React from "react";
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+import { Link } from "react-router-dom";
 
 const PDFUpload = () => {
-    const [pdfFile, setPDFFile] = useState(null)
-    const [viewPDF, setViewPDF] = useState(null)
-    const newplugin = defaultLayoutPlugin()
-    
-    const handleChange = (e) => {
-        let selectedFile = e.target.files[0]
+  const [pdfFile, setPDFFile] = useState(null);
+  const [viewPDF, setViewPDF] = useState(null);
+  const newplugin = defaultLayoutPlugin();
 
-        if (selectedFile.type.includes('application/pdf')) {
-            let reader = new FileReader()
-            reader.readAsDataURL(selectedFile)
-            reader.onload = (e) => {
-                setPDFFile(e.target.result)
-            }
-        } else {
-            setPDFFile(null)
-        }
+  const handleChange = (e) => {
+    let selectedFile = e.target.files[0];
+
+    if (selectedFile.type.includes("application/pdf")) {
+      let reader = new FileReader();
+      reader.readAsDataURL(selectedFile);
+      reader.onload = (e) => {
+        setPDFFile(e.target.result);
+      };
+    } else {
+      setPDFFile(null);
     }
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        if(!pdfFile) {
-            alert(`Please choose a file to upload.`)
-
-        } else if (!pdfFile.includes('application/pdf')) {
-            alert(`Please select a PDF File.`)
-        } else if(pdfFile !== null) {
-            setViewPDF(pdfFile)
-        } else {
-            setViewPDF(null)
-        }
+    if (!pdfFile) {
+      alert(`Please choose a file to upload.`);
+    } else if (!pdfFile.includes("application/pdf")) {
+      alert(`Please select a PDF File.`);
+    } else if (pdfFile !== null) {
+      setViewPDF(pdfFile);
+    } else {
+      setViewPDF(null);
     }
+  };
 
-    return (
-        <>
-            <h3>Please Upload a PDF File</h3>
+  return (
+    <>
+      <h3>Please Upload a PDF File</h3>
 
-            <form onSubmit={handleSubmit}>
-                <label htmlFor='upload'>Upload a File: </label>
-                <input type='file' name='upload' onChange={handleChange}/>
-                <br></br>
-                <button type='submit'>Upload</button>
-            </form>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="upload">Upload a File: </label>
+        <input type="file" name="upload" onChange={handleChange} />
+        <br></br>
+        <button type="submit">Upload</button>
+      </form>
 
-            <br></br>
+      <br></br>
 
-            <div>
-                <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js'>
-                    {viewPDF &&
-                    <>
-                        <Viewer fileUrl={viewPDF} plugins={[newplugin]}/>
-                    </>}
-                    {!viewPDF && <>No PDF Uploaded</>}
-                </Worker>
-            </div>
+      <div>
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+          {viewPDF && (
+            <>
+              <Viewer fileUrl={viewPDF} plugins={[newplugin]} />
+            </>
+          )}
+          {!viewPDF && <>No PDF Uploaded</>}
+        </Worker>
+      </div>
 
-            <br></br>
+      <br></br>
 
-            <Link to='/translate'>Select a New File</Link>
-        </>
-    )
-}
+      <Link to="/translate">Select a New File</Link>
+    </>
+  );
+};
 
-export default PDFUpload
+export default PDFUpload;
